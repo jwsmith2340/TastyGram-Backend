70771efb23dfea08333534793ba88b66999ce570
Micahlou <micahlouis99@gmail.com>
2022-01-27 20:10:53 -0500

    added delete and update routes to recipes controller

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -29,4 +29,26 @@ recipesRouter.post("/", async (req, res) => {
     }
 });
 
+// Recipe Delete Route
+recipesRouter.delete("/:id", async (req, res) => {
+    try {
+        // send all recipes
+        res.json(await Recipe.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+});
+
+// Recipe Update Route
+recipesRouter.put("/:id", async (req, res) => {
+    try {
+        // send all recipes
+        res.json(await Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+});
+
 module.exports = recipesRouter;
