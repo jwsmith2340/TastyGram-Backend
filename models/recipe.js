694dede357752fb90e126f92a3a4601c70445c93
Micahlou <micahlouis99@gmail.com>
2022-01-28 11:18:48 -0500

    began creation of recipes array and removed name string from recipe schema

--- models/recipe.js
+++ models/recipe.js
@@ -4,7 +4,6 @@ const schema = mongoose.Schema;
 const recipeSchema = new mongoose.Schema({
     title: String,
     image: String,
-    name: String,
     description: String,
     ingredients: String,
 }, {timestamps: true});
