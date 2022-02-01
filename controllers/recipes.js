c767ab5e4deda6d3d3b388a425dac041b41e76e3
Micahlou <micahlouis99@gmail.com>
2022-02-01 00:31:47 -0500

    added auth to backend

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,6 +3,27 @@ const recipesRouter = express.Router();
 const Recipe = require('../models/recipe')
 const recipesArr = require ('../models/recipesArr.json')
 
+recipesRouter.use(async function(req, res, next) {
+    try{
+        const token = req.get('Authorization');
+        if(!token) return next();
+
+        const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+        if(!user) throw new Error('Something went wrong');
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
+
+
+function isAuthenticated(req, res, next) {
+    if(!req.user) return res.status(401).json({message: 'You must be logged in first'})
+    next();
+}
+
 // test route
 // recipesRouter.get("/", (req, res) => {
 //     res.send("Hello World");
@@ -24,7 +45,8 @@ recipesRouter.get("/", (req, res) => {
 })
 
 // Recipe Create Route
-recipesRouter.post("/", async (req, res) => {
+recipesRouter.post("/", isAuthenticated, async (req, res) => {
+    
     try {
         // send all recipes
         res.json(await Recipe.create(req.body));
--- server.js
+++ server.js
@@ -4,11 +4,22 @@ const app = express();
 const morgan = require("morgan");
 const cors = require("cors")
 const mongoose = require("mongoose")
+const admin = require("firebase-admin");
+
 const methodOverride = require('method-override')
 const expressSession = require('express-session')
 require("dotenv").config();
 // Pull PORT && MONGODB_URL
-const { PORT, MONGODB_URL, SECRET } = process.env;
+const { PORT, MONGODB_URL, GOOGLE_CREDENTIALS } = process.env;
+
+
+
+const serviceAccount = JSON.parse(GOOGLE_CREDENTIALS);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
 
 // Controllers
 const recipesController = require('./controllers/recipes');
@@ -30,6 +41,28 @@ app.use(cors()); // to prevent cors errors, open access to all origins
 app.use(morgan("dev")); // logging
 app.use(express.json()); // parse json bodies
 app.use(methodOverride('_method'));
+
+// app.use(async function(req, res, next) {
+//     try{
+//         const token = req.get('Authorization');
+//         if(!token) return next();
+
+//         const user = await admin.auth().verifyIdToken(token.replace('Bearer ', ''));
+//         if(!user) throw new Error('Something went wrong');
+
+//         req.user = user;
+//         next();
+//     } catch (error) {
+//         res.status(400).json(error);
+//     }
+// });
+
+
+// function isAuthenticated(req, res, next) {
+//     if(!req.user) return res.status(401).json({message: 'You must be logged in first'})
+//     next();
+// }
+
 
 // Routes
 // test route
